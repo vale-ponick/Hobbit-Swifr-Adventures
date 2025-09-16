4ca246a387a8b5b333149a026bb2ef47f1e026ad
Valery Ponomareva <vale.ponick@gmail.com>
2025-09-16 08:00:16 +0700

    "chore: add project structure for Hobbit adventure game"

---
+++ Hobbit-Swift-Adventures/Sources/Logics/GameManager.swift
@@ -0,0 +1,7 @@
+//
+//  GameManager.swift
+//  Hobbit-Swift-Adventures
+//
+//  Created by Валерия Пономарева on 16.09.2025.
+//
+
---
+++ Hobbit-Swift-Adventures/Sources/Views/MainView.swift
@@ -0,0 +1,7 @@
+//
+//  MainView.swift
+//  Hobbit-Swift-Adventures
+//
+//  Created by Валерия Пономарева on 16.09.2025.
+//
+
